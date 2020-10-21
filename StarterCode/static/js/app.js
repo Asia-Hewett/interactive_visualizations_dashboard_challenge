1fc9d9a51711304359db530998387717938a90b7
Asia-Hewett <asia.hewett0046@gmail.com>
2020-10-21 12:57:07 -0500

    I'm Lost

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,15 +1,10 @@
-// function unpack(rows, index) {
-//     return rows.map(function(row) {
-//       return row[index];
-//     });
-//   }
-
 // Use the D3.js library to read in the JSON
 // Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
 // Use sample_values as the values for the bar chart.
 // Use otu_ids as the labels for the bar chart.
 // Use otu_labels as the hovertext for the chart.
 
+// Generating the data for the drop down menu
 
 d3.json("../../data/samples.json").then((subjectData) => {
     dropDown(subjectData);
@@ -24,13 +19,39 @@ function dropDown(subjectData) {
 
     // dropdown menu to display the top 10 OTUs found in that individual
     subjectValue.forEach((name) => {
-        let selectionVal = name;
+        let selectionName = name;
         let options = dropDown.append("option");
-        options.text(selectionVal);
-        options.attr("value", name);
-    
+        options.text(selectionName);
+        options.attr("optionChanged", name);
+        
     });
 };
+
+// Creating the event handler for the dropdown menu
+
+d3.selectAll("#selDataset").on("change", updatePage);
+
+function updatePage() {
+    let dropDownMenu = d3.selectAll("#selDataset");
+    let sampleValues = dropDownMenu.subjectData.sample_values;
+    let otuIds = dropDownMenu.subjectData.otu_ids;
+    let otuLabels = dropDownMenu.subjectData.otu_labels;
+    console.log(sampleValues);
+    console.log(otuIds);
+    console.log(otuLabels)
+
+    let trace1 = [{
+        type: 'bar',
+        x: [20, 14, 23],
+        y: ['giraffes', 'orangutans', 'monkeys'],
+        orientation: 'h'
+      }];
+      
+      Plotly.newPlot('bar', data);
+
+    Plotly.newPlot();
+}
+
 
 
 
