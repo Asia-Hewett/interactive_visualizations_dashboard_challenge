853e21ff4d432a0fad51c822fe5fde5b2a1ff414
Asia-Hewett <asia.hewett0046@gmail.com>
2020-10-20 21:16:53 -0500

    Added Dropdown & Options Menu

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,30 @@
-d3.json("../../data/samples.json").then((data) => {
-    // mydropDown(data);
-    console.log(data)
-});  
+// function unpack(rows, index) {
+//     return rows.map(function(row) {
+//       return row[index];
+//     });
+//   }
+
+// // Read the json, assign it to a variable then call the function to populate the dropdown menu
+d3.json("../../data/samples.json").then((subjectData) => {
+    dropDown(subjectData);
+    // console.log(subjectData)
+});
+
+// Populate the dropdown with the data from json
+function dropDown(subjectData) {
+    
+    //Grab the test subject ID # from the data (names field)
+    let subjectValue = subjectData.names;
+    let dropDown = d3.select("#selDataset");
+    dropDown.html("");
+
+    //Populate the dropdown options with the sample "names" (test subject ID #)
+    subjectValue.forEach((name) => {
+        let selectionVal = name;
+        let options = dropDown.append("option");
+        options.text(selectionVal);
+        options.attr("value", name);
+    
+    });
+};
+
